f7cc6836d03da3fafa6de37109f0fe9149dbe31a
Lazakovich Valery <lazakovichvalery@outlook.com>
2017-04-07 14:57:04 +0300

    Encrease Angular version to 4

--- ang-src/config/webpack.common.js
+++ ang-src/config/webpack.common.js
@@ -50,13 +50,13 @@ module.exports = {
   },
 
   plugins: [
-    // Workaround for angular/angular#11580
     new webpack.ContextReplacementPlugin(
-      // The (\\|\/) piece accounts for path separators in *nix and Windows
-      /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
-      helpers.root('./src'), // location of your src
-      {} // a map of your routes
-    ),
+        /angular(\\|\/)core(\\|\/)@angular/,
+        helpers.root('src'), // location of your src
+        {
+          // your Angular Async Route paths relative to this root directory
+        }
+      ),
 
     new webpack.optimize.CommonsChunkPlugin({
       name: ['app', 'vendor', 'polyfills']
